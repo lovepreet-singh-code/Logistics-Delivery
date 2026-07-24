8806d78fbfacec373ce9fd34f1a721e94150e59f
lovepreet-singh-code <info.thelovepreetsingh@gmail.com>
2026-07-24 14:48:16 +0530

    update npm bcrypt pakage

--- identity-service/src/models/User.ts
+++ identity-service/src/models/User.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, Document, Model } from "mongoose";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { IUser, UserRole } from "../@types";
 
 // ──── Mongoose Document Type ────
